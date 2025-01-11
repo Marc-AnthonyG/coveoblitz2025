60b0965a52ba38a5e9acb8c04c71d8e5e5dd1ea3
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2025-01-11 13:42:41 -0500

    feat: new file

---
+++ evitement.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import game_message
+
+
+# def allo(game_message: game_message.TeamGameState, action: List[game_message.Action]) -> List[game_message.Action]:
+#     return True
+
+
+# if move.type == "MOVE_TO":
+#     move_to_position: Position = move.position
+#     if util.is_not_in_enemies_zone(game_message.teamIds, game_message.currentTeamId, move_to_position,
+#                                    game_message.teamZoneGrid) \
+#             and util.is_not_in_enemies_zone(game_message.teamIds, game_message.currentTeamId, character.position,
+#                                             game_message.teamZoneGrid):
+#         self.current_state[character.id] = rcr.strategy_state.STACK_RESOURCES
+# else:
+#     character_actions.append(move)
