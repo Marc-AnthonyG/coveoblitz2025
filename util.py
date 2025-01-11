35f93ad4d4a4e99c18345e9859d24340e6beee40
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2025-01-11 15:37:07 -0500

    feat: v55

--- util.py
+++ util.py
@@ -8,7 +8,7 @@ def is_in_our_zone(current_team_id: str, position: game_message.Position,  teamZ
     return teamZoneGrid[position.x][position.y] == current_team_id
 
 def is_not_in_enemies_zone(teamIds: list[str], current_team_id: str, position: game_message.Position,  teamZoneGrid: list[list[str]]) -> bool:
-    return teamZoneGrid[position.x][position.y] not in teamIds or teamZoneGrid[position.x][position.y] == current_team_id
+    return not is_in_enemies_zone(teamIds, current_team_id, position, teamZoneGrid)
 
 def is_in_enemies_zone(teamIds: list[str], current_team_id: str, position: game_message.Position,  teamZoneGrid: list[list[str]]) -> bool:
     return teamZoneGrid[position.x][position.y] in teamIds and teamZoneGrid[position.x][position.y] != current_team_id
