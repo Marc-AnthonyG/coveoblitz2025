756bcbec6b330c50798bee43cb6d75d6e28ea506
Nathaniel Girard <nathaniel.girard1@gmail.com>
2025-01-11 10:49:50 -0500

    fixed bugs

--- attack.py
+++ attack.py
@@ -1,6 +1,7 @@
 from game_message import *
 from util import is_not_in_enemies_zone, is_in_enemies_zone
 from collections import deque
+from retrieve_closest_resource import is_tile_empty
 
 def choose_to_pickup_or_deposit(character: Character, game_state: TeamGameState) -> list[Action]:
     if len(character.carriedItems) > 0 and character.carriedItems[-1].value < 0:
@@ -19,7 +20,7 @@ def pickupTrash(character: Character, game_state: TeamGameState) -> list[Action]
 
 
 def depositTrash(character: Character, game_state: TeamGameState) -> list[Action]:
-    if is_in_enemies_zone(game_state.teamIds, game_state.currentTeamId, character.position, game_state.teamZoneGrid) and game_state.map.tiles[character.position.x][character.position.y] == TileType.EMPTY:
+    if is_in_enemies_zone(game_state.teamIds, game_state.currentTeamId, character.position, game_state.teamZoneGrid) and game_state.map.tiles[character.position.x][character.position.y] == TileType.EMPTY and is_tile_empty(character.position, game_state):
         return [DropAction(character.id)]
 
 
@@ -30,7 +31,7 @@ def depositTrash(character: Character, game_state: TeamGameState) -> list[Action
 
     while queue:
         current_position = queue.popleft()
-        if is_in_enemies_zone(game_state.teamIds, game_state.currentTeamId, current_position, game_state.teamZoneGrid):
+        if is_in_enemies_zone(game_state.teamIds, game_state.currentTeamId, current_position, game_state.teamZoneGrid) and is_tile_empty(current_position, game_state):
             return [MoveToAction(character.id, Position(current_position.x, current_position.y))]
         adjacent_positions = [
                 Position(current_position.x + 1, current_position.y),
@@ -38,7 +39,9 @@ def depositTrash(character: Character, game_state: TeamGameState) -> list[Action
                 Position(current_position.x, current_position.y + 1),
                 Position(current_position.x, current_position.y - 1)
             ]
-        adjacent_positions = [pos for pos in adjacent_positions if 0 <= pos.x < game_state.map.width and 0 <= pos.y < game_state.map.height and game_state.map.tiles[pos.x][pos.y] == TileType.EMPTY]
+        adjacent_positions = [pos for pos in adjacent_positions if 0 <= pos.x < game_state.map.width and 
+                              0 <= pos.y < game_state.map.height and 
+                              game_state.map.tiles[pos.x][pos.y] == TileType.EMPTY]
         (game_state.map.width, game_state.map.height)
         for neighbor in adjacent_positions:
                 
