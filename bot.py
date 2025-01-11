32dc2a8bdefdfc1e61f0fa0f0de21b2e5fb8d6ec
Nathaniel Girard <nathaniel.girard1@gmail.com>
2025-01-11 11:01:17 -0500

    merged main

--- bot.py
+++ bot.py
@@ -13,22 +13,28 @@ class Bot:
         print("Initializing your super mega duper bot")
 
     def get_next_move(self, game_message: TeamGameState):
-        if not self.weight_map:
-            self.weight_map = construct_weighted_map(game_message)
+        # if not self.weight_map:
+        #     self.weight_map = construct_weighted_map(game_message)
 
         if (len(self.current_state) == 0):
-            for character in game_message.yourCharacters:
-                self.current_state[character.id] = rcr.strategy_state.RETRIEVE_CLOSEST_RESOURCE
+            for i, character in enumerate(game_message.yourCharacters):
+                if i % 2 == 0:
+                    self.current_state[character.id] = None
+                else:
+                    self.current_state[character.id] = rcr.strategy_state.RETRIEVE_CLOSEST_RESOURCE
+                
         
         """
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
         actions = []
-        actions += choose_to_pickup_or_deposit(game_message.yourCharacters[0], game_state=game_message)
 
-        # for character in game_message.yourCharacters:
-        #     move, next_state = rcr.make_a_move(self.current_state[character.id], character, game_message)
-        #     self.current_state[character.id] = next_state
-        #     actions.append(move)
+        for character in game_message.yourCharacters:
+            if self.current_state[character.id] is None:
+                actions += choose_to_pickup_or_deposit(character, game_state=game_message)
+            else:
+                move, next_state = rcr.make_a_move(self.current_state[character.id], character, game_message)
+                self.current_state[character.id] = next_state
+                actions.append(move)
         # You can clearly do better than the random actions above! Have fun!
         return actions
