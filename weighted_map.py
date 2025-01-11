491ee9660b16d7f541e094666bb906f9c0488fe4
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2025-01-11 10:30:47 -0500

    feat: yolo

--- weighted_map.py
+++ weighted_map.py
@@ -1,14 +1,23 @@
 from collections import deque
+from dataclasses import dataclass
 from typing import List
 
-from game_message import TileType, Threat, Position
+from game_message import TileType, TeamGameState, Position
 
 
-def construct_weighted_map(map: List[List[TileType]], spawn_position: Position) -> list[list[float]]:
+@dataclass
+class WeightedMap:
+    """Weighted map for calcul."""
+    weighted_map: list[list[float]]
+    prioritize_drop_point: list[Position]
+    prioritize_defence_point: list[Position]
+
+def construct_weighted_map(game_message: TeamGameState) -> WeightedMap:
     """
     Construct a list of list with float point to values tiles. The goal is to have 0 on tiles inaccessible by the player like walls
     and 1 on the tiles where you can go in the most place in 3 turns.
     """
+    map: List[List[TileType]] = game_message.map.tiles
     width = len(map)
     height = len(map[0])
 
@@ -19,7 +28,7 @@ def construct_weighted_map(map: List[List[TileType]], spawn_position: Position) 
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
     # BFS helper function to explore reachable tiles in 3 moves from a given position
-    def bfs(start_x: int, start_y: int, dont_stop: bool = False) -> set[tuple[int, int]]:
+    def bfs(start_x: int, start_y: int) -> set[tuple[int, int]]:
         queue = deque([(start_x, start_y, 0)])  # (x, y, distance)
         visited = {(start_x, start_y)}
         accessible_tiles = 0
@@ -27,8 +36,8 @@ def construct_weighted_map(map: List[List[TileType]], spawn_position: Position) 
         while queue:
             x, y, dist = queue.popleft()
 
-            # Stop searching if we've moved more than 3 turns
-            if dist >= 10 and not dont_stop:
+            # Stop searching if we've moved more than 10 turns
+            if dist >= 10:
                 continue
 
             # Explore adjacent tiles
@@ -45,13 +54,13 @@ def construct_weighted_map(map: List[List[TileType]], spawn_position: Position) 
     max_reachable_tiles = 0
     reachable_map_count = [[0] * height for _ in range(width)]
 
-    visitable_tiles = bfs(spawn_position.x, spawn_position.y, True)
-    for x, y in visitable_tiles:
-        if map[x][y] != TileType.WALL:
-            reachable_tiles_number = len(bfs(x, y))
-            reachable_map_count[x][y] = reachable_tiles_number
-            max_reachable_tiles = max(max_reachable_tiles, reachable_tiles_number)
-            min_reachable_tiles = min(min_reachable_tiles, reachable_tiles_number)
+    for x in range(width):
+        for y in range(width):
+            if map[x][y] != TileType.WALL:
+                reachable_tiles_number = len(bfs(x, y))
+                reachable_map_count[x][y] = reachable_tiles_number
+                max_reachable_tiles = max(max_reachable_tiles, reachable_tiles_number)
+                min_reachable_tiles = min(min_reachable_tiles, reachable_tiles_number)
 
 
     for x in range(width):
@@ -63,4 +72,16 @@ def construct_weighted_map(map: List[List[TileType]], spawn_position: Position) 
                         max_reachable_tiles - min_reachable_tiles))  # Reachable tiles weight
                 )
 
-    return weighted_map_normilize
+
+    weighted_zone = []
+    for x in range(width):
+        for y in range(height):
+            if game_message.teamZoneGrid[x][y] == game_message.currentTeamId and weighted_map_normilize[x][y] != 0:
+                weighted_zone.append(Position(x, y))
+
+    weighted_zone.sort(key=lambda position: weighted_map_normilize[position.x][position.y])
+
+    defense_list = weighted_zone.copy()
+    defense_list.reverse()
+
+    return WeightedMap(weighted_map=weighted_map_normilize, prioritize_drop_point=weighted_zone, prioritize_defence_point=defense_list)
